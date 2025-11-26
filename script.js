e9062be750609c5fcf776d0dbeaea8fc810cca37
Xytricit <andohbempahnanaakwasi@gmail.com>
2025-11-26 23:00:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const links = document.querySelector('.links');
+
+hamburger.addEventListener('click', () => {
+    links.classList.toggle('active');
+});
 
