f7f6393ae4313a2ac911cea5f20809de57828031
Xytricit <andohbempahnanaakwasi@gmail.com>
2025-11-27 11:14:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".mobile-menu");
 
-// Toggle menu when hamburger is clicked
 hamburger.addEventListener("click", () => {
     mobileMenu.classList.toggle("active");
 });
 
-// Close menu when clicking outside (not logo!)
 document.addEventListener("click", (e) => {
     if (
         mobileMenu.classList.contains("active") &&
