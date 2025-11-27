df5a0b6a506b0a23575692b3b359ea8636d2c9ec
Xytricit <andohbempahnanaakwasi@gmail.com>
2025-11-27 09:00:49 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const mobileMenu = document.querySelector(".mobile-menu");
 hamburger.addEventListener("click", () => {
     mobileMenu.classList.toggle("active");
 });
+document.querySelector(".mobile-menu .logo").addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+});
